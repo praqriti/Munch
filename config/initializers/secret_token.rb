7ae1f0a2bd9f26267cc8102eda09e309980c35d3
Prakriti <prakriti@prakriti-Inspiron-1545.(none)>
2011-03-26 00:07:38 +0530

    starting a fresh copy of munch

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Munch::Application.config.secret_token = '6addeca30801178a4db05464b5e1415f8d3a38ebfd9e71142409ae34b943e1e1c626beef4e2574a4965d0799300c219ff4abdc62aa03d8f80871179c40b23d4e'
